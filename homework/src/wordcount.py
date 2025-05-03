a718461e0dc619793424be09ff0bbe4862e0b6ec
Mario Pineda <mapinedal@unal.edu.co>
2025-05-03 11:00:47 -0500

    Segunda corrección mover funciones a internals

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,8 +1,8 @@
-from .._internals.count_words_1 import count_words
-from .._internals.preprocess_lines import preprocess_lines
-from .._internals.read_all_lines import read_all_lines
-from .._internals.split_in_words import split_in_words
-from .._internals.write_count_words import write_count_words
+from ._internals.count_words_1 import count_words
+from ._internals.preprocess_lines import preprocess_lines
+from ._internals.read_all_lines import read_all_lines
+from ._internals.split_in_words import split_in_words
+from ._internals.write_count_words import write_count_words
 
 
 def main():
